74ddd38a1a2d3277fbce4f6f130b29eea898a915
joanna-tan <55102022+joanna-tan@users.noreply.github.com>
2022-12-09 02:15:56 -0500

    Add files via upload

--- blast_local.py
+++ blast_local.py
@@ -1,83 +1,115 @@
 import re
 import glob
 import csv
+from Bio import SeqIO
 from Bio.Blast.Applications import NcbiblastpCommandline
 from Bio.Blast import NCBIXML
 
 E_VALUE_THRESH = 1E-5
 
 """
-Method that returns the protein_id, gene, and locus_tag in a string.
+Method that returns the protein_id, gene, protein, and locus_tag in a string.
 """
 def find_tags(str):
     protein_id_reg = re.compile(r'\[protein_id=([A-Za-z0-9.]+)\]')
     gene_reg = re.compile(r'\[gene=([A-Za-z0-9]+)\]')
+    protein_reg = re.compile(r'\[protein=([A-Za-z0-9/ ,-]+)\]')
     locus_tag_reg = re.compile(r'\[locus_tag=([A-Za-z0-9_]+)\]')
 
     prot_search = protein_id_reg.search(str)
     protein_id = prot_search.group(1) if prot_search else None
     gene_search = gene_reg.search(str)
     gene = gene_search.group(1) if gene_search else None
+    protein_search = protein_reg.search(str)
+    protein = protein_search.group(1) if protein_search else None
     locus_search = locus_tag_reg.search(str)
     locus_tag = locus_search.group(1) if locus_search else None
 
-    return protein_id, gene, locus_tag
+    return protein_id, gene, protein, locus_tag
 
 """
 Method that parses BLAST output.
 """
 def parse_blast_output(blast_output, formatted_output):
+    matches = set()
+    formatted_output += "_core_" + str(E_VALUE_THRESH) + ".csv"
+
     with open (formatted_output, 'w') as csvoutput:
         writer = csv.writer(csvoutput, lineterminator='\n')
         all_out = []
 
         for record in NCBIXML.parse(open(blast_output)):
             if record.alignments:
-                q_protein, q_gene, q_locus = find_tags(record.query)
-                line = "[protein_id={protein}] [gene={gene}] [locus_tag={locus}]" \
-                        .format(protein=q_protein, gene=q_gene, locus=q_locus)
-                count = 0
+                q_protein_id, q_gene, q_protein, q_locus = find_tags(record.query)
+                line = "[locus_tag={locus}]&[protein_id={protein_id}]&[gene={gene}]&[protein={protein}]" \
+                        .format(protein_id=q_protein_id, gene=q_gene, protein=q_protein, locus=q_locus)
+                curr_hits = 0
+                matches.add(q_locus)
+                
                 for align in record.alignments:
                     for hsp in align.hsps:
-                        if hsp.expect < E_VALUE_THRESH and count < 3:
-                            protein_id, gene, locus_tag = find_tags(align.title)
-                            line += " [protein_id={protein}] [gene={gene}] [locus_tag={locus}] [evalue={expect:.5E}]" \
-                                    .format(protein=protein_id, gene=gene, locus=locus_tag, expect=hsp.expect)
-                            count += 1
-                line = line.split(" ")
+                        if hsp.expect < E_VALUE_THRESH and curr_hits < 3:
+                            protein_id, gene, protein, locus_tag = find_tags(align.title)
+                            line += "&[locus_tag={locus}]&[protein_id={protein_id}]&[gene={gene}]&[protein={protein}]&[evalue={expect:.5E}]" \
+                                    .format(protein_id=protein_id, gene=gene, protein=protein,locus=locus_tag, expect=hsp.expect)
+                            curr_hits += 1
+                            
+                line = line.split("&")
                 all_out.append(line)
 
-                writer.writerows(all_out)
+        writer.writerows(all_out)
+    
+    return len(all_out), matches
 
-    # parse_file = open(formatted_output, "w")
-    # for record in NCBIXML.parse(open(blast_output)):
-    #     if record.alignments:
-    #         q_protein, q_gene, q_locus = find_tags(record.query)
-    #         parse_file.writelines("\nQUERY: [protein_id=%s] [gene=%s] [locus_tag=%s]" %(q_protein, q_gene, q_locus))
-    #         for align in record.alignments:
-    #             for hsp in align.hsps:
-    #                 if hsp.expect < E_VALUE_THRESH:
-    #                     protein_id, gene, locus_tag = find_tags(align.title)
-    #                     parse_file.writelines("\nMATCH: [protein_id=%s] [gene=%s] [locus_tag=%s] [evalue=%.5E]" %(protein_id, gene, locus_tag, hsp.expect))
-    #         parse_file.writelines("\n")
+"""
+Method that finds the unique genes, given a set of matched loci.
+"""
+def find_unique(input_file, matches, formatted_output):
+    formatted_output += "_unique_" + str(E_VALUE_THRESH) + ".csv"
+    locus_tag_reg = re.compile(r'\[locus_tag=([A-Za-z0-9_]+)\]')
+    uniques = []
+
+    with open(input_file, "r") as handle:
+        for record in SeqIO.parse(handle, "fasta"):
+            locus_search = locus_tag_reg.search(record.description)
+            locus = locus_search.group(1) if locus_search else None
+
+            if locus not in matches:
+                q_protein_id, q_gene, q_protein, q_locus = find_tags(record.description)
+                uniques.append((locus, q_gene, q_protein, q_locus))
+
+    with open(formatted_output, 'w') as csvoutput:
+        writer = csv.writer(csvoutput, lineterminator='\n')
+        all_out = []
+
+        for q_protein_id, q_gene, q_protein, q_locus in uniques:
+            line = "[locus_tag={locus}]&[protein_id={protein_id}]&[gene={gene}]&[protein={protein}]" \
+                        .format(protein_id=q_protein_id, gene=q_gene, protein=q_protein, locus=q_locus)
+            line = line.split("&")
+            all_out.append(line)
+
+        writer.writerows(all_out)
+
+    return uniques
 
 """
 Method that runs local BLAST of query to a database.
 """
 def blast_local(query, database):
-    # cline = NcbiblastpCommandline(cmd = "/usr/local/ncbi/blast/bin/blastp", query=database, db="nr",
-    #                         evalue=0.001, outfmt=5, out = "output.xml")
     query_name = query.split("_")[0]
     db_name = database.split("/")[-1]
     blast_output = "{query_name}_to_{db_name}.txt".format(query_name = query_name, db_name=db_name)
-    formatted_output = "{query_name}_to_{db_name}.csv".format(query_name = query_name, db_name=db_name)
+    formatted_output = "{query_name}_to_{db_name}".format(query_name = query_name, db_name=db_name)
     cline = NcbiblastpCommandline(query=query, db=database,
                                 evalue=E_VALUE_THRESH, outfmt=5, out = blast_output)
     print(cline)
     stdout, stderr = cline()
 
-    parse_blast_output(blast_output, formatted_output)
     
+    hits, matches = parse_blast_output(blast_output, formatted_output)
+    uniques = find_unique(query, matches, formatted_output)
+
+    return hits, len(uniques), matches
     
 if __name__ == "__main__":
     # # Running from the thesis parent directory python3 blast_local.py
@@ -85,12 +117,18 @@ if __name__ == "__main__":
     # db = "MED4/MED4"
 
     # Running from the query directory: python3 ../blast_local.py
-    # input_file = "MED4_GCA_000011465.1_ASM1146v1_translated_cds.faa"
-    for input_file in glob.glob("*.faa"):
-        db = "../MED4/MED4"
-        blast_local(input_file, db)
+    # for input_file in glob.glob("*.faa"):
+    #     db = "../MED4/MED4"
+    #     hits, uniques, matches = blast_local(input_file, db)
+    #     genes = len([1 for line in open(input_file) if line.startswith(">")])
+    #     print("Number of hits: {hits} \nNumber of uniques: {uniques} \nNumber of queries: {genes}".format(hits=hits, uniques=uniques, genes=genes))
+    #     print("% matches: {matches:.2F}%".format(matches=hits/genes * 100))
 
     # BLAST against MIT9312 database
     for input_file in glob.glob("*.faa"):
         db = "../MIT9312/MIT9312"
-        blast_local(input_file, db)
+        hits, uniques, matches = blast_local(input_file, db)
+        # hits = len(matches)
+        genes = len([1 for line in open(input_file) if line.startswith(">")])
+        print("Number of hits: {hits} \nNumber of uniques: {uniques} \nNumber of queries: {genes}".format(hits=hits, uniques=uniques, genes=genes))
+        print("% matches: {matches:.2F}%".format(matches=hits/genes * 100))
